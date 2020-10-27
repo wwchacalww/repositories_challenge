ae3ef34053a9a60f5dd2dc425bda9836b7bf1ab8
Vicente Cartaxo <juniorcartaxo@gmail.com>
2020-10-26 23:04:24 -0300

    Route updated done.

--- src/app.js
+++ src/app.js
@@ -25,7 +25,29 @@ app.post("/repositories", (request, response) => {
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params;
+
+  const { title, url, techs } = request.body;
+
+  const repositoryIndex = repositories.findIndex(repository => repository.id === id );
+
+  if ( repositoryIndex < 0 ) {
+    return response.status(401).json({ error : "Repository not found."});
+  }
+
+  const likes = repositories[repositoryIndex].likes;
+
+  const repository = {
+    id,
+    title,
+    url,
+    techs,
+    likes
+  }
+
+  repositories[repositoryIndex] = repository;
+
+  return response.status(200).json(repository);
 });
 
 app.delete("/repositories/:id", (request, response) => {
